88f7258310b8f442fdc6552ce5e196411c61a68f
hicham <zyadnih@gmail.com>
2024-07-11 15:31:57 +0100

    second page

--- src/_metronic/layout/components/CoursDetailComponent/CourCardList.tsx
+++ src/_metronic/layout/components/CoursDetailComponent/CourCardList.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const CourCardList = () => {
     const coursCards = [
         {
+            
             icon: "ki-book-open",
             statusText: "Terminé",
             statusColor: "success", // Green color for "Terminé"
---
+++ src/_metronic/layout/components/CoursDetailComponent/CoursComponenent.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+
+const CoursComponenent = () => {
+  return (
+    <div>
+      <div id="kt_app_content" className="app-content flex-column-fluid">
+							{/*begin::Content container*/}
+							<div id="kt_app_content_container" className="app-container container-fluid">
+								{/*begin::Row*/}
+						<div className="row g-5 g-xl-8">
+
+							{/*begin::Col*/}
+							<div className="col-xl-12">
+								{/*begin::Row*/}
+								<div className="row gx-5 gx-xl-8 mb-5 mb-xl-8">
+								
+                  <h1>hello word</h1>
+							</div>
+								{/*end::Row*/}
+								{/*begin::Row*/}
+							
+								{/*end::Row*/}
+							</div>
+							{/*end::Col*/}
+						</div>
+						{/*end::Row*/}
+							</div>
+							{/*end::Content container*/}
+						</div>
+    </div>
+  )
+}
+
+export default CoursComponenent
